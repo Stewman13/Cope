53091be5fc20f78d04aedff14b297e1f00aae58d
Vanja Sorokin <tsukimizuki69@gmail.com>
2015-07-01 05:43:02 +1000

    Big Guy In
    anims working, and pathing working.

---
+++ Assets/TheBigGuy/MoveTowards.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowards : MonoBehaviour {
+
+	public GameObject Animator;
+	public Animator anim;
+
+	public Transform CurrentCheckpoint;
+	public GameObject Checkpoint1;
+	public GameObject Checkpoint2;
+	public GameObject Checkpoint3;
+	public GameObject Checkpoint4;
+
+	public bool ShouldMove = false;
+
+	public GameObject me;
+	
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		anim = Animator.GetComponent<Animator>();
+		anim.SetBool("isHappyRunning",true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	void Move(){
+		if(ShouldMove == true){
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, CurrentCheckpoint.position, step);
+		}
+	}
+}
