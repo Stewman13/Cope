07ca977d285a41ef7dae35f514d55b8906bd017d
Stewman13 <110367@student.sae.edu.au>
2015-07-02 09:32:09 +1000

    new stuff
    Yes indeedy

---
+++ Assets/Scripts/FalllingEndThingymabobagigit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FalllingEndThingymabobagigit : MonoBehaviour {
+
+	public GameObject Earthqauke;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player"){
+			Earthqauke.rigidbody.isKinematic = false;
+			Earthqauke.rigidbody.useGravity = true;
+			Earthqauke.audio.Play();
+		}
+	}
+}
