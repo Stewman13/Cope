84dcfa8ab699f60fe5891aaa38f9ede4c180d4c5
Vanja Sorokin <tsukimizuki69@gmail.com>
2015-07-02 06:58:54 +1000

    Even More? No way!
    indeed

---
+++ Assets/Scripts/Menus/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	public Transform Checkpoint1;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, Checkpoint1.position, step);
+	}
+}
---
+++ Assets/Scripts/Menus/WaitForNextScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitForNextScene : MonoBehaviour {
+
+	public float WaitTime = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(WaitAndPrint(2.5F));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator WaitAndPrint(float waitTime) {
+		yield return new WaitForSeconds(WaitTime);
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
